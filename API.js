32754035e9bed73e8eb5e1022ac1a08c7c28ca49
Sasidharan G S <SasidharanGS.701@gmail.com>
2023-08-30 12:14:49 +0530

    final

---
+++ API.js
@@ -0,0 +1,91 @@
+var base = "https://inshortsv2.vercel.app/news?type=";
+var postfix = "&limit=27";
+var type = "all_news";
+var temp;
+var more = 0, start = 5, end = 10;
+
+function change(cat){
+    window.scrollTo(0, 0);
+    document.getElementById("cat").innerHTML = cat.innerHTML;
+    document.getElementById("read-more").innerHTML = "Read More";
+    if(cat.innerHTML === "Home"){
+        temp = "all_news"
+    }
+    else{
+        temp = cat.innerText.toLowerCase();
+    }
+    type = temp;
+    
+    Load();
+}
+
+function Load(){
+    more = 0; start = 5; end = 10;
+    async function API() {
+        let response = await fetch(base+type+postfix);
+        let data = await response.json();
+        return data;
+    }
+    
+    API().then( data => {
+        var news = document.getElementById("news");
+        news.innerHTML = "";
+        for(var i=0; i<10; i++){
+            news.innerHTML += `<div class="card">
+                <div class="row">
+                    <div width="30%" class="column1">
+                        <img id="img${i+1}" src="" width="100%" margin-left="0px" alt="">
+                    </div>
+                    <div width="70%" class="column2">
+                        <h4 id="head${i+1}"></h4>
+                        <p id="txt${i+1}"></p>
+                    </div>
+                </div>
+            </div>`;
+            document.getElementById(`head${i+1}`).innerHTML = data.articles[i].title;
+            document.getElementById(`img${i+1}`).src = data.articles[i].image_url;
+            document.getElementById(`txt${i+1}`).innerHTML = data.articles[i].description;
+        }
+        if(counter % 2 !== 0){
+            dark();
+        }
+    });
+}
+
+function loadMore(){
+    if(more < 3){
+        more++;
+        start+=5;end+=5;
+        async function API() {
+            let response = await fetch(base+type+postfix);
+            let data = await response.json();
+            return data;
+        }
+        
+        API().then( data => {
+            for(var i=start; i<end; i++){
+                var news = document.getElementById("news");
+                news.innerHTML += `<div class="card">
+                <div class="row">
+                    <div width="30%" class="column1">
+                        <img id="img${i+1}" src="" width="100%" margin-left="0px" alt="">
+                    </div>
+                    <div width="70%" class="column2">
+                        <h4 id="head${i+1}"></h4>
+                        <p id="txt${i+1}"></p>
+                    </div>
+                </div>
+                </div>`;
+                document.getElementById(`head${i+1}`).innerHTML = data.articles[i].title;
+                document.getElementById(`img${i+1}`).src = data.articles[i].image_url;
+                document.getElementById(`txt${i+1}`).innerHTML = data.articles[i].description;
+            }
+            if(counter % 2 !== 0){
+                dark();
+            }
+        });
+    }
+    else{
+        document.getElementById("read-more").innerHTML = "Unable to load more"
+    }
+}
---
+++ README.md
@@ -0,0 +1,48 @@
+# The Oberver
+
+## About
+
+A website which displays live and updated news
+
+## Tools used
+
+- Vanila HTML, CSS, Javascript
+- Inshorts News API v2
+   - [This API's documentation](https://github.com/sumitkolhe/inshorts-api-v2)
+
+## Visit the site
+
+Visit the website [here](https://vinayak5002.github.io/Observer/home.html)
+
+## Features:
+- Live updates of news
+- Read news from 10 different catogories 
+- Pleasing dark mode 
+- Rsponsive website which can be used it almost any device
+- You can load more news if you are intrested (There is a limit of how many news will be loaded btw)
+
+## Previews
+
+### PC Version
+
+Home page in light mode
+
+![](readme/light%20home.jpg)
+
+Home page in dark mode
+
+![](readme/dark%20home.jpg)
+
+### Mobile Version
+
+- Landing page light mode
+
+![image](https://user-images.githubusercontent.com/82216732/128609907-65a40017-a9b2-4eff-8390-9e4b1b799572.png)
+
+- Landing page in Dark mode
+
+![image](https://user-images.githubusercontent.com/82216732/128609942-14c503f4-e5fb-4d9e-84b8-1410d8995a5f.png)
+
+- Navigation bar in action
+
+![image](https://user-images.githubusercontent.com/82216732/128610031-d4605eba-3589-42e7-bef4-e0990bed3531.png)
